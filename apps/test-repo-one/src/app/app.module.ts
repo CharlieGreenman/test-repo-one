a429094679feb490654b922f1ca7525e0a9246eb
Charlie Greenman <charlie@razroo.com>
2023-10-29 21:25:51 -0400

    Add code for auth-0-angular-16.2.0 integrate-auth-module.
    [Url: razroo.com/razroo/auth-0-angular-16.2.0/create-auth-core-module/integrate-auth-module]

--- apps/test-repo-one/src/app/app.module.ts
+++ apps/test-repo-one/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthCoreModule } from "@test-repo-one/common/common-services";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AuthCoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
